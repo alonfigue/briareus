5c15cb95388b1d7ffbc7a73b94fe2cfa921fdf10
alonfigue <alonsogfm2000@gmail.com>
2022-04-19 22:41:45 -0400

    Table done

--- briareus/src/App.js
+++ briareus/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React, { useState } from 'react';
+import { TaskRow } from './components/TaksRow';
+
 
 function App() {
+
+
+  const [username, setUserName] = useState('agfm');
+  const [taskItems, setTaskItems] = useState([
+    { name: 'Task one', done: false },
+    { name: 'Task two', done: false },
+    { name: 'Task three', done: true },
+    { name: 'Task four', done: false }
+  ])
+
+
+  const toggleTask = task =>
+    setTaskItems(taskItems.map(t => (t.name == task.name ? { ...t, done: !t.done } : t)))
+
+
+  const taskTableRows = () => (
+    taskItems.map(task => (
+      <TaskRow task={task} key={task.name} toggleTrakie={toggleTask}></TaskRow>
+    ))
+  )
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Briareus</h1>
+      <table className='table table-striped table-bordered'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+
+        <tbody>{taskTableRows()}</tbody>
+      </table>
     </div>
   );
 }
